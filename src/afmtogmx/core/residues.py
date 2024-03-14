bfcfdff3667becf35fa82a4ad0c1be66210c8791
rjweldon <rjweldon@uark.edu>
2024-03-14 13:39:18 -0500

    Add fxn to check residue_priority

--- src/afmtogmx/core/residues.py
+++ src/afmtogmx/core/residues.py
@@ -44,3 +44,22 @@ def _check_residue_atnums(bonded, residue_atnums):
             print(f"Atom numbers {not_in_off} defined in residue_atnums are not found in molname {molname}. Check your "
                   f"input files")
             exit(1)
+
+
+def _check_molname_resname(bonded, residues, residue_priority):
+    """Checks to see if molnames, resnames defined in residue_priority are present in self.residues
+
+    :param bonded: self.bonded
+    :param residues: self.residues
+    :param residue_priority: dictionary
+    """
+    for molname, priorities in residue_priority.items():
+        if molname not in bonded:
+            print(f"\nMolname {molname} from residue_priority not found in .off file. Please check residue_priority "
+                  f"input")
+            exit(1)
+        for resname in priorities:
+            if resname not in residues['Definitions'][molname] or resname not in residues['Residues'][molname]:
+                print(f"\nResidue name {resname} not found in defined off.residues dictionary. Check residue_priority "
+                      f"input")
+                exit(1)
